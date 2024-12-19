3f9e248860cac48bafbd7644b2af48ac4bf6f404
VorTx828 <Sp060504@gmail.com>
2024-12-19 15:15:32 +0500

    Исправление ошибок

--- Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
+++ Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372fba18c8ac7d42c2652fc1699767c7d050114e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e7d64f8ff903032531725e303ec018cb7f32f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
